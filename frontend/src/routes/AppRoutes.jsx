1e1f5c88e9551e872ce4aebd5fb9da00e367540f
brunomartin <bruno.pedraza@conexa.ai>
2022-07-05 16:00:55 -0300

    new repository

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import './App.css';
+
+function AppRoutes() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route/>
+          <Route/>
+          <Route/>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
